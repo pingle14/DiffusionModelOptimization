7a400d23d2d428c542896602f564982b392e8e10
Prachi Ingle <prachi.ingle@utexas.edu>
2024-11-27 16:56:15 -0600

    Updated column headers and script to generate correct headers

--- diffusion_model/sampler.py
+++ diffusion_model/sampler.py
@@ -33,7 +33,7 @@ def noising_timestep_order(timesteps):
 
 
 # Euler sampling function
-def euler_sampler(model, xt, time_steps=[], device='cuda'):
+def euler_sampler(model, xt, time_steps=[], device="cuda"):
     xtraj = [xt.clone()]
 
     # time_steps = generative_denoising_timestep_order(time_steps)
@@ -45,12 +45,14 @@ def euler_sampler(model, xt, time_steps=[], device='cuda'):
             # TODO: note that will need to fiddle with dimensions of this tensor
             # TODO: fix step/ t stufffff
             step_t = time_steps[:, i].unsqueeze(-1)
- 
+
             t = step_t
-            #print(t.shape) #TODO: Handle non-tensor case with : torch.full((xt.shape[0], 1), fill_value=step, device=device)
+            # print(t.shape) #TODO: Handle non-tensor case with : torch.full((xt.shape[0], 1), fill_value=step, device=device)
             all_ones = torch.ones_like(step_t, device=device)
             denominator = (
-                time_steps[:, i+1].unsqueeze(-1) if i < num_time_steps - 1 else all_ones
+                time_steps[:, i + 1].unsqueeze(-1)
+                if i < num_time_steps - 1
+                else all_ones
             ) - step_t
             # print(denominator)
             step_size = denominator
@@ -61,7 +63,7 @@ def euler_sampler(model, xt, time_steps=[], device='cuda'):
 
             xtraj.append(xt.clone().detach())
 
-    return xt#, xtraj
+    return xt  # , xtraj
 
 
 # # # Generate images
@@ -78,13 +80,14 @@ def euler_sampler(model, xt, time_steps=[], device='cuda'):
 # df = pd.DataFrame(data)
 # noise = pd.DataFrame(original_noise.cpu().numpy())
 # df = pd.concat([df, noise], axis=1)
-# df.to_csv("data.csv")
+# df.rename(columns=['X1', 'X2', 'Z1', 'Z2], inplace=True)
+# df.to_csv("data.csv", index=False)
 # print(data.shape)
 
 # plt.scatter(data[:, 0], data[:, 1])
 # plt.savefig(f"fig.png")
 
-# =---------- END 
+# =---------- END
 
 # for i in range(10):
 #     plt.close()
